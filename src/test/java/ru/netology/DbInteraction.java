115e0e1f17d2c7a2871e8119c4f47b0f26582303
NataliaGracheva <40542294+NataliaGracheva@users.noreply.github.com>
2020-04-15 20:01:59 +0500

    utils class added

--- src/test/java/ru/netology/DbInteraction.java
+++ src/test/java/ru/netology/DbInteraction.java
@@ -1,41 +1,26 @@
 package ru.netology;
 
-import lombok.val;
-import org.apache.commons.dbutils.QueryRunner;
-import org.apache.commons.dbutils.handlers.ScalarHandler;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import ru.netology.mode.DbUtils;
 import ru.netology.mode.User;
+import ru.netology.page.DashboardPage;
 import ru.netology.page.LoginPage;
 import ru.netology.page.VerificationPage;
 
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
 import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DbInteraction {
     User user = new User("vasya", "qwerty123");
 
     @Test
     @DisplayName("Логин с валидными данными")
-    void loginWithValidData() throws SQLException {
+    void loginWithValidData() {
         open("http://localhost:9999");
         LoginPage loginPage = new LoginPage();
         VerificationPage verificationPage = loginPage.validLogin(user);
-
-        val codeSQL = "SELECT code FROM auth_codes WHERE created = (SELECT max(created) FROM auth_codes);";
-        val runner = new QueryRunner();
-
-        try (
-                val conn = DriverManager.getConnection(
-                        "jdbc:mysql://localhost:3306/app", "app", "pass"
-                );
-        ) {
-            val code = runner.query(conn, codeSQL, new ScalarHandler<>());
-            System.out.println(code);
-
-            verificationPage.validVerify((String) code);
-        }
+        DashboardPage dashboardPage = verificationPage.validVerify(DbUtils.getVerificationCode());
+        assertEquals("Личный кабинет", dashboardPage.getHeading());
     }
 }
--- src/test/java/ru/netology/page/DashboardPage.java
+++ src/test/java/ru/netology/page/DashboardPage.java
@@ -3,11 +3,17 @@ package ru.netology.page;
 import com.codeborne.selenide.SelenideElement;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 
 public class DashboardPage {
     private SelenideElement heading = $("[data-test-id=dashboard]");
     public DashboardPage() {
-        heading.shouldHave(text("Личный кабинет"));
+//        heading.shouldHave(text("Личный кабинет"));
+        heading.shouldBe(visible);
+    }
+
+    public String getHeading() {
+        return heading.getText().trim();
     }
 }
